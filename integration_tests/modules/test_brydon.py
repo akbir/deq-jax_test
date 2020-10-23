updated deq for correct gradients

integration_tests/modules/test_brydon.py
@@ -2,39 +2,49 @@ import jax.numpy as jnp
 import jax
 import numpy as np
 import torch
-
+import pytest
 from deq_torch.brydon import th_broyden, th_rmatvec, th_matvec
 from deq_jax.src.modules.broyden import broyden, rmatvec, matvec
 
+RNG = np.random.default_rng(42)
+
+def quadratic(x):
+    return x ** 2 + x - 5
+
+def sinsodial(x):
+    if type(x) == torch.Tensor:
+        return np.sin(x)
+    return jnp.sin(x)
+
+def linear(x):
+    w, b = 2, 1
+    return w * x + b
 
 class TestBroydenIntegration:
-    def test_optima(self):
-        def quadratic(x):
-            return x ** 2
-
-        values = np.random.rand(2, 3, 4)
+    @pytest.mark.parametrize('fun', [quadratic, sinsodial, linear])
+    def test_optima(self, fun):
+        values = RNG.random((2, 3, 4))
         threshold = 30
-        eps = 1e-6
-        th_input = torch.tensor(values, dtype=torch.float32)
-        jax_input = jnp.asarray(values, dtype=jnp.float32)
+        eps = 1e-6 * np.sqrt(values.size)
+        jax_broyden = jax.jit(broyden, static_argnums=(0, 2, 3))
 
-        broyden_jit = jax.jit(broyden, static_argnums=(0, 2, 3))
+        jax_ans = jax_broyden(jax.jit(fun), jnp.asarray(values, dtype=jnp.float32), threshold, eps)
+        th_ans = th_broyden(fun, torch.tensor(values, dtype=torch.float32), threshold, eps)
 
-        jax_ans = broyden_jit(jax.jit(quadratic), jax_input, threshold, eps)
-        th_ans = th_broyden(quadratic, th_input, threshold, eps)
+        # check they could converge
+        assert jax_ans['prot_break'] == th_ans['prot_break']
 
+        # check models are equal
+        np.testing.assert_array_almost_equal(jax_ans['trace'][:jax_ans['n_step'] + 1], th_ans['trace'], decimal=4)
         np.testing.assert_array_almost_equal(np.array(jax_ans['result']),
                                              th_ans['result'].numpy(),
-                                             decimal=6)
+                                             decimal=4)
 
-        # assert jax_ans['n_step'] == th_ans['nstep']
-        assert jax_ans['prot_break'] == th_ans['prot_break']
+        assert jax_ans['n_step'] == th_ans['nstep']
         assert jax_ans['eps'] == th_ans['eps']
 
-        np.testing.assert_array_almost_equal(jax_ans['diff'], th_ans['diff'], decimal=6)
-        np.testing.assert_array_almost_equal(np.array(jax_ans['diff_detail']), th_ans['diff_detail'].numpy(), decimal=5)
-        np.testing.assert_array_almost_equal(jax_ans['trace'], th_ans['trace'], decimal=6)
-
+        np.testing.assert_array_almost_equal(jax_ans['diff'], th_ans['diff'], decimal=4)
+        np.testing.assert_array_almost_equal(np.array(jax_ans['diff_detail']), th_ans['diff_detail'].numpy(), decimal=4)
 
     def test_rmatvec(self):
         for _ in range(10):
@@ -50,20 +60,16 @@ class TestBroydenIntegration:
             np.testing.assert_almost_equal(np.array(rmatvec(ja_2, ja_3, ja)),
                                            th_rmatvec(th_2, th_3, th).numpy(), decimal=5)
 
-
     def test_matvec(self):
         for _ in range(10):
-            values = np.random.rand(2, 3, 4)
+            values = RNG.random((2, 3, 4))
             th = torch.tensor(values, dtype=torch.float32)
             ja = jnp.asarray(values, dtype=jnp.float32)
 
-            values_2 = np.random.rand(2, 3, 4, 5)
-            values_3 = np.random.rand(2, 5, 3, 4)
+            values_2 = RNG.random((2, 3, 4, 5))
+            values_3 = RNG.random((2, 5, 3, 4))
             th_2, th_3 = torch.tensor(values_2, dtype=torch.float32), torch.tensor(values_3, dtype=torch.float32)
             ja_2, ja_3 = jnp.asarray(values_2, dtype=jnp.float32), jnp.asarray(values_3, dtype=jnp.float32)
 
             np.testing.assert_almost_equal(np.array(matvec(ja_2, ja_3, ja)),
                                            th_matvec(th_2, th_3, th).numpy(), decimal=5)
-
-
-

integration_tests/modules/test_rootfind.py
@@ -1,81 +1,167 @@
 import copy
 
+import haiku as hk
+import jax
 import jax.numpy as jnp
 import numpy as np
 import torch
-from jax import value_and_grad, grad
 import torch.nn as nn
+from jax import value_and_grad, grad
 
-from deq_jax.src.modules.rootfind import rootfind
+from deq_jax.src.modules.rootfind import deq
 from deq_torch.rootfind import DEQModule, RootFind
 
 
-class TestRootFindIntegration:
-    def test_parity():
-        def layer(x, w):
-            return x * w
-        def toy_model(params, data):
-            z_star = rootfind(layer, data, 30, params)
-            y = layer(z_star, params)
-            return jnp.sum(y)
+class Pointwise(nn.Module):
+    def __init__(self, weight, bias):
+        super().__init__()
+        self.weight = torch.nn.Parameter(weight * torch.ones(1, 2, 3), requires_grad=True)
+        self.register_parameter(name='W', param=self.weight)
 
-        params = -2 * jnp.ones((1,2,3))
-        data = jnp.ones((1,2,3))
-        value, grad_params = value_and_grad(toy_model)(params, data)
-        grad_inputs = grad(toy_model, argnums=1)(params, data)
+        self.bias = torch.nn.Parameter(bias * torch.ones(1, 2, 3), requires_grad=True)
+        self.register_parameter(name='B', param=self.bias)
 
-        assert (-6.0 == value or 0. == value)
+    def forward(self, z1ss, *args):
+        return self.weight * z1ss + self.bias
 
-        class DumbLinear(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.weight = torch.nn.Parameter(-2 * torch.ones(1, 2, 3), requires_grad=True)
-                self.register_parameter(name='W', param=self.weight)
+    def copy(self, func):
+        self.weight.data = func.weight.data.clone()
+        self.bias.data = func.bias.clone()
 
-            def forward(self, z1ss, *args):
-                return z1ss * self.weight
+class TLinear(nn.Module):
+    def __init__(self, output_size, weight, bias):
+        super().__init__()
+        self.linear = nn.Linear(output_size, output_size, bias=True)
+        with torch.no_grad():
+            self.linear.weight.fill_(weight)
+            self.linear.bias.fill_(bias)
 
-            def copy(self, func):
-                self.weight.data = func.weight.data.clone()
+    def forward(self, z1ss, *args):
+        return self.linear(z1ss)
 
-        class linearDEQ(DEQModule):
-            def __init__(self, func, func_copy):
-                super(linearDEQ, self).__init__(func, func_copy)
-
-            def forward(self, z1s):
-                zero = torch.zeros_like(z1s)
-                threshold, train_step = 30, -1
-                z1s_out = RootFind.apply(self.func, z1s, zero, zero, zero, threshold, train_step)
-                if self.training:
-                    z1s_out = RootFind.f(self.func, z1s_out, zero, zero, zero, threshold, train_step)
-                    z1s_out = self.Backward.apply(self.func_copy, z1s_out, zero, zero, zero, threshold, train_step)
-                return z1s_out
+    def copy(self, func):
+        self.linear.weight.data = func.linear.weight.clone()
+        self.linear.bias.data = func.linear.bias.clone()
 
 
-        class DEQ(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.func = DumbLinear()
-                self.func_copy = copy.deepcopy(self.func)
-                for params in self.func_copy.parameters():
-                    params.requires_grad_(False)
+class LinearDEQ(DEQModule):
+    def __init__(self, func, func_copy):
+        super(LinearDEQ, self).__init__(func, func_copy)
 
-                self.deq = linearDEQ(self.func, self.func_copy)
+    def forward(self, z1s):
+        zero = torch.zeros_like(z1s)
+        threshold, train_step = 30, -1
+        z1s_out = RootFind.apply(self.func,
+                                 z1s,
+                                 None,
+                                 None,
+                                 None,
+                                 threshold,
+                                 train_step)
 
-            def forward(self, input):
-                self.func_copy.copy(self.func)
-                return self.deq(input)
-
-        th_net = DEQ()
-        input = torch.ones(1,2,3)
-        input.requires_grad = True
-        torch_value = th_net(input).sum()
-        torch_value.backward()
-        torch_gradient = th_net.func.W.grad
-
-        np.testing.assert_almost_equal(np.asarray(value), torch_value.detach().numpy())
-        np.testing.assert_almost_equal(np.asarray(grad_inputs), input.grad.detach().numpy())
-        np.testing.assert_almost_equal(np.asarray(grad_params), torch_gradient.detach().numpy())
+        z1s_out = RootFind.f(self.func, z1s_out, zero, zero, zero, threshold, train_step)
+        z1s_out = self.Backward.apply(self.func_copy, z1s_out, zero, zero, zero, threshold, train_step)
+        return z1s_out
 
 
+def test_parity_with_jax():
+    weight = -3
+    bias = 10
 
+    def layer(params, x):
+        return params['w'] * x + params['b']
+
+    def toy_model(params, data):
+        z = deq(layer, 30, params, data, True)
+        return jnp.sum(z)
+
+    params = {'w': weight * jnp.ones((1, 2, 3)), 'b': bias * jnp.ones((1, 2, 3))}
+    data = jnp.ones((1, 2, 3))
+
+    value, grad_params = value_and_grad(toy_model)(params, data)
+    grad_input = grad(toy_model, argnums=1)(params, data)
+
+    class DEQ(nn.Module):
+        def __init__(self, weight, bias):
+            super().__init__()
+            self.func = Pointwise(weight, bias)
+            self.func_copy = copy.deepcopy(self.func)
+            for params in self.func_copy.parameters():
+                params.requires_grad_(False)
+
+            self.deq = LinearDEQ(self.func, self.func_copy)
+
+        def forward(self, input):
+            self.func_copy.copy(self.func)
+            return self.deq(input)
+
+    th_net = DEQ(weight, bias)
+    input = torch.ones(1, 2, 3, requires_grad=True)
+
+    torch_value = th_net(input).sum()
+    torch_value.backward()
+    torch_w_gradient = th_net.func.W.grad
+    torch_b_gradient = th_net.func.B.grad
+
+    np.testing.assert_almost_equal(value, torch_value.detach().numpy())
+    np.testing.assert_almost_equal(grad_input, input.grad.detach().numpy())
+    np.testing.assert_almost_equal(np.asarray(grad_params['b']), torch_b_gradient.detach().numpy())
+    np.testing.assert_almost_equal(np.asarray(grad_params['w']), torch_w_gradient.detach().numpy())
+
+
+def test_parity_with_haiku():
+    weight, bias = 1, 5
+
+    def build_forward(output_size, max_iter):
+        def forward_fn(x: jnp.ndarray) -> jnp.ndarray:
+            linear_1 = hk.Linear(output_size,
+                                 name='l1',
+                                 w_init=hk.initializers.Constant(weight),
+                                 b_init=hk.initializers.Constant(bias))
+
+            transformed_linear = hk.without_apply_rng(
+                hk.transform(linear_1)
+            )
+            inner_params = hk.experimental.lift(
+                transformed_linear.init)(hk.next_rng_key(), x)
+
+            return deq(transformed_linear.apply, max_iter, inner_params, x, True)
+        return forward_fn
+
+    input = 2*jnp.ones((1, 2, 3))
+    rng = jax.random.PRNGKey(42)
+    forward_fn = build_forward(3, 30)
+    forward_fn = hk.transform(forward_fn)
+    params = forward_fn.init(rng, input)
+
+    # @jax.jit
+    def loss_fn(params, rng, x): return jnp.sum(forward_fn.apply(params, rng, x))
+
+    value = loss_fn(params, rng, input)
+    grad_params = grad(loss_fn)(params, rng, input)
+
+    class DEQ(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.func = TLinear(3, weight, bias)
+            self.func_copy = copy.deepcopy(self.func)
+            for params in self.func_copy.parameters():
+                params.requires_grad_(False)
+            self.deq = LinearDEQ(self.func, self.func_copy)
+
+        def forward(self, input):
+            self.func_copy.copy(self.func)
+            # return self.deq(input)
+            return self.deq(input)
+
+    th_net = DEQ()
+    input = 2 * torch.ones(1, 2, 3, requires_grad=True)
+
+    torch_value = th_net(input).sum()
+    torch_value.backward()
+    torch_w_gradient = th_net.func.linear.weight.grad
+    torch_b_gradient = th_net.func.linear.bias.grad
+
+    np.testing.assert_almost_equal(value, torch_value.detach().numpy())
+    np.testing.assert_almost_equal(np.asarray(grad_params['lifted/l1']['b']), torch_b_gradient.detach().numpy())
+    np.testing.assert_almost_equal(np.asarray(grad_params['lifted/l1']['w']), torch_w_gradient.detach().numpy())